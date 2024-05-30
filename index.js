de386f8b05747a09b34761427728558edfc4f3c5
madhan <rajagopalamadhan@gmail.com>
2024-05-30 21:52:39 +0530

    view and exit function are created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = 8000;
 const Pool = require('pg').Pool;
 const bodyparser = require('body-parser');
 const path = require('path');
+const parse = require('parse');
 
 const app = express();
 
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 pool.query(
-    `CREATE TABLE IF NOT EXISTS student (name VARCHAR(50),rollnumber VARCHAR(50),email VARCHAR(50),department VARCHAR(50))`,
+    `CREATE TABLE IF NOT EXISTS student (name VARCHAR(50),rollnumber VARCHAR(50) PRIMARY KEY,email VARCHAR(50),department VARCHAR(50))`,
     (err, result) => {
         if (err) {
             console.error('Error creating table', err.stack);
@@ -36,15 +37,44 @@ pool.query(
 
 app.post('/submit', (req, res) => {
     const { name, rollnumber, email, department } = req.body;
+    let details;
     pool.query('INSERT INTO student (name, rollnumber, email, department) VALUES ($1, $2, $3, $4)', [name, rollnumber, email, department], (err, result) => {
         if (err) {
             res.status(500).send('Error inserting data');
             return console.error('Error executing query', err.stack);
         }
-        res.send('Data inserted successfully');
+        res.send('Inserted successfully');
     });
 });
 
+app.get('/students', (req, res) => {
+    pool.query(`SELECT * FROM student`, (err, result) => {
+        if(err){
+            res.send('Error occured while displaying');
+        }
+        res.json(result.rows);
+    });
+});
+
+
+app.get('/view', (req, res) => {
+    res.sendFile(path.join(__dirname, 'responce', 'view.html'));
+});
+
+app.get('/delete', (req, res) => {
+    res.sendFile(path.join(__dirname, 'responce', 'deleteElement.html'));
+});
+
+app.get('/exit', (req, res) => {
+    pool.query(`drop table student`, (err, result) => {
+        if (err) {
+            console.error('Error creating table', err.stack);
+        } else {
+            console.log('Table is ready or already exists');
+        }
+    });
+    res.send('Thank you for accessing our webpage');
+});
 
 
 app.listen(port, ()=> {
